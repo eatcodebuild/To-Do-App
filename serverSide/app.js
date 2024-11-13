6b27ffe88838a94f54ff1d726b8e374232880b83
Ryan Jeffrey <r.jeffrey@live.com.au>
2024-11-13 20:56:03 +1100

    jj

--- serverSide/app.js
+++ serverSide/app.js
@@ -160,7 +160,7 @@ app.get('/success', (req, res) => {
 
 
 // test for deployment
-app.get('/test', (req, res) => {
+app.get('/tester', (req, res) => {
     res.status(200).json('hello, this is working')
 });
 
