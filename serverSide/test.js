5e99ae38415fb0791937539046777cc557dae96a
Ryan Jeffrey <r.jeffrey@live.com.au>
2024-11-13 21:58:59 +1100

    added mongoose.connect back in

--- serverSide/test.js
+++ serverSide/test.js
@@ -24,11 +24,11 @@ app.set('views', path.join(__dirname, 'views'));
 
 
 
-// // // MongoDB Connection                                                                      // ***** there is an issue in here preventing me from deploying on vercel *****s
-// mongoose.connect(process.env.MONGODB, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// });
+// MongoDB Connection                                                                      // ***** there is an issue in here preventing me from deploying on vercel *****s
+mongoose.connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB Connection Error:'));
 
